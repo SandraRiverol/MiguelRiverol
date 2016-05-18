4c32a15125ca5b9abb29619a907fcdee81317b27
SandraRiverol <cecylitta1@hotmail.com>
2016-05-18 11:10:29 -0500

    Saludos random

--- hola.php
+++ hola.php
@@ -1,4 +1,6 @@
 <?php 
-	echo 'Hola';
-	echo 'Hola gato';
+	$str= array("Hola gatitos","Hola crayola","Holo... Espera, hola","Mamá prende la licuadora","Buenos días",
+	"¿Donde está tu honor, basura?","Hola compañero","Hola osito","asdfgjkdfjgkrkgs","Saludos, camarada","Heil Hitler");
+	$a = rand(0,10);
+	echo $str[$a];
 ?>
